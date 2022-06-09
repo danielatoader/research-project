c1124d67a49f62982c62b2f0ea60616ab356f8c5
Daniela Toader <daniela.toader07@gmail.com>
2022-06-09 14:29:29 +0200

    Reorder methods, add main, add more effect size stats

--- src/process_evosuite_data.py
+++ src/process_evosuite_data.py
@@ -17,7 +17,12 @@ class ProcessEvoSuiteData:
     def print_name(self):
         os.system(f"echo The name is: {self.name}")
 
-    def calculate_medians(self, search_budget, columns_to_group, score_metric, score_metric_filename):
+    def calculate_medians(
+            self,
+            search_budget,
+            columns_to_group,
+            score_metric,
+            score_metric_filename):
         """
         Extract results from the EvoSuite results csv files with given search bugdet.
         """
@@ -51,8 +56,58 @@ class ProcessEvoSuiteData:
         res_medians = pd.read_csv(medians_filename)
         
         return res_medians
+    
+    def get_ck_metrics(self):
+        """
+        Will be used as features for the model.
+        """
 
-    def get_significant_classes_stats(self, output_csv, search_budget, compared_function, columns_to_group, score_metric):
+        class_metrics = pd.read_csv("ck_data/class.csv")
+        class_metrics = class_metrics.iloc[:, 1:]
+        return class_metrics
+    
+    def get_matching_classes_metrics(
+        self,
+        output_csv,
+        search_budget,
+        columns_to_group,
+        score_metric):
+        """
+        Matches what the ck tool measured on the SF110 with the classes in EvoSuite output files.
+        """
+        
+        class_metrics = self.get_ck_metrics()
+
+        # Features are the metrics themselves: cbo, loc, etc.
+        features = np.array(class_metrics.columns.values)[2:]
+
+        # Get data and columns to work with
+        res = pd.read_csv(output_csv)
+        configuration_ids = ['weak_' + str(search_budget), 'branch_' + str(search_budget), 'default_' + str(search_budget)]
+        all_columns = columns_to_group + [score_metric]
+
+        # Configuration column name is the second in the columns_to_group list
+        config_column = columns_to_group[1]
+        
+        # Class column name is the first in the columns_to_group list
+        class_column = columns_to_group[0]
+
+        # Sort by configuration
+        res = res.loc[:,all_columns]
+        result = res[res.apply(lambda row : row[config_column] in configuration_ids, axis=1)]
+
+        # Match classes from ck tool result with classes in res_data (EvoSuite's output)
+        matching_classes = class_metrics[class_metrics['class'].isin(result[class_column])].drop_duplicates()
+ 
+        return matching_classes
+
+    def get_significant_classes_stats(
+            self,
+            output_csv,
+            search_budget,
+            compared_function,
+            columns_to_group, 
+            score_metric):
         """
         Get Wilcoxon and Vargha-Delaney statistics for all classes and return both significant and all classes along with their stats.
         """
@@ -107,6 +162,9 @@ class ProcessEvoSuiteData:
         for ((key1, val1), (key2, val2)) in zip(weak_classes.items(), compared_func_classes.items()):
 
             val1, val2 = pad(val1, val2)
+
+            # If both weak and compared score metrics are equal, then flag with (-2,-2)
+            # Otherwise perform Wilcoxon and Vargha-Delaney
             stats_p = (-2,-2) if (np.sum(np.subtract(val1, val2)) == 0) else wilcoxon(val1, val2)
             vd = VD_A(val1.tolist(), val2.tolist())
             class_stats[key1[0]] = (stats_p, vd)
@@ -114,53 +172,20 @@ class ProcessEvoSuiteData:
 
         significant_class_stats = dict()
 
+        # Filter classes with 'p' < 0.05 and a large effect size 
         for (key, ((stats, p), vd)) in class_stats.items():
-            if (p > -2 and p < 0.05 and vd[1] == 'large'):
+            if (p > -2 and p < 0.005 and vd[1] == 'large'):
                 significant_class_stats[key] = ((stats, p), vd) 
         
-        # Return statistically significant class stats & all classes stats (stats = p-values)
+        # Return statistically significant class stats & all classes stats
         return significant_class_stats, class_stats
 
-    def get_ck_metrics(self):
-        """
-        Will be used as features for the model.
-        """
-
-        class_metrics = pd.read_csv("ck_data/class.csv")
-        class_metrics = class_metrics.iloc[:, 1:]
-        return class_metrics
-    
-    def get_matching_classes_metrics(self, output_csv, search_budget, columns_to_group, score_metric):
-        """
-        Matches what the ck tool measured on the SF110 with the classes in EvoSuite output files.
-        """
-        
-        class_metrics = self.get_ck_metrics()
-
-        # Features are the metrics themselves: cbo, loc, etc.
-        features = np.array(class_metrics.columns.values)[2:]
-
-        # Get data and columns to work with
-        res = pd.read_csv(output_csv)
-        configuration_ids = ['weak_' + str(search_budget), 'branch_' + str(search_budget), 'default_' + str(search_budget)]
-        all_columns = columns_to_group + [score_metric]
-
-        # Configuration column name is the second in the columns_to_group list
-        config_column = columns_to_group[1]
-        
-        # Class column name is the first in the columns_to_group list
-        class_column = columns_to_group[0]
-
-        # Sort by configuration
-        res = res.loc[:,all_columns]
-        result = res[res.apply(lambda row : row[config_column] in configuration_ids, axis=1)]
-
-        # Match classes from ck tool result with classes in res_data (EvoSuite's output)
-        matching_classes = class_metrics[class_metrics['class'].isin(result[class_column])].drop_duplicates()
- 
-        return matching_classes
-
-    def get_significant_classes_matching_metrics(self, search_budget, columns_to_group, score_metric, score_metric_filename):
+    def get_significant_classes_matching_metrics(
+            self,
+            search_budget,
+            columns_to_group,
+            score_metric,
+            score_metric_filename):
         """ 
         Get significant classes and their corresponding matching metrics.
         
@@ -213,5 +238,4 @@ class ProcessEvoSuiteData:
         matched_metrics = matching_classes_metrics[key_class_metrics]
         signif_matched_metrics[key_class_metrics] = matched_metrics[matched_metrics.apply(lambda row : row['class'] in significant_classes, axis=1)]
 
-        return signif_matched_metrics  
-    
+        return signif_matched_metrics
